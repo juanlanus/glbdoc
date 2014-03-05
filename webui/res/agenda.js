1ea05354cd57b69e6d24e325005d71190c89f72c
Juan Lanus <juan.lanus@gmail.com>
2014-03-05 16:48:42 -0300

    0305 glb initial doc

---
+++ webui/res/agenda.js
@@ -0,0 +1,9 @@
+        $(function() {
+          $('#tocContainer').tocBuilder({ 
+            type: 'headings', 
+            backLinkText: 'TOC', 
+            startLevel: 2, 
+            endLevel: 6
+          });
+        });
+
