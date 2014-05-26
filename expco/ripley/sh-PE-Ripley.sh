fe69de9a7e5486aa848469204cca8ca4c1f5d915
Juan Lanus <juan.lanus@gmail.com>
2014-05-26 08:11:40 -0300

    0526 home

--- expco/ripley/sh-PE-Ripley.sh
+++ expco/ripley/sh-PE-Ripley.sh
@@ -1,42 +1,79 @@
 # /d/iccol/desarrollo/macros/sh-PE-Ripley
 #sh-PE-Ripley.V.1.0001
-#**********************************************************
+#*******************************************************************************
 #SHELL-ID:     sh-PE-Ripley
 #DATE-WRITTEN: 2014/05/23
 #LAST-UPDATE:  2014/05/23
 #AUTHOR:       Globant - JL
-#**********************************************************
+#*******************************************************************************
+# Definición de constantes
+NITSuscriptor=900047981
+
+# Definición de parámetros 
+# nombre del archivo de entrada
+Archivo="pe.txt"
+
+paramsGet() {
+#*******************************************************************************
+# Lee los parámetros de la corrida anterior
+#*******************************************************************************
+#   asegura que existe el arch de parámetros general
+    [ ! -f $0.parm ] && touch $0.parm
+#   lee los parámetros, generales o del archivo actual
+    if [ -z Archivo ]
+    then
+        params=`cat $Archivo.parm`
+    else
+        params=`cat $0.parm`
+    fi
+    echo ">>>> params: " $params
+#   separa los parámetros según su orden
+    Archivo=`echo params | cut -d ";" -f1`
+    Archivo=`echo params | cut -d ";" -f1`
+    Archivo=`echo params | cut -d ";" -f1`
+}
+
+echo ">>>>>>>>>>>" $0.parm
+echo ">>>>>>>>>>>" $Archivo
+echo ">>>>>>>>>>>" `basename $Archivo .txt`
+
+
+paramsGet
+# DEBUG: termina acá
+exit
+
+
 
 mostrar_parametros() {
-#***********************************************************************
-#mostrar_parametros.
-#   Lee y muestra los parametros de ventacruzada para un suscriptor
-#***********************************************************************
-  perams=`cat $cadena01.parm`
-  base=`echo $perams  | cut -d ";" -f1`
-  entfmt=`echo $perams  | cut -d ";" -f2`
-  fecha=`echo $perams  | cut -d ";" -f3`
-  echo "+-----------------------------------------------------------+"
-  echo "| *** PARAMETROS PROCESO PE FALABELLA SEMESTRAL ***         |"
-  echo "|       Base (I=interna, E=externa)     :  " $base
-  echo "|       Formato(1=1-11,2=1-11-45)       :  " $entfmt
-  echo "|       Fecha de Proceso                :  " $fecha
-  echo "+-----------------------------------------------------------+"
-  echo "Si desea modificar algun parametro digite S ---> \c"
-  read modpard
-  if test -z "$modpard"
-   then
-    modpar="N"
-  else
-    if [ $modpard == "s" ] || [ $modpard == "S" ]; then
-       modpar="S"
+    #*******************************************************************************
+    #mostrar_parametros.
+    #   Lee y muestra los parametros de ventacruzada para un suscriptor
+    #*******************************************************************************
+    perams=`cat $cadena01.parm`
+    base=`echo $perams  | cut -d ";" -f1`
+    entfmt=`echo $perams  | cut -d ";" -f2`
+    fecha=`echo $perams  | cut -d ";" -f3`
+    echo "+-----------------------------------------------------------+"
+    echo "| *** PARAMETROS PROCESO PE FALABELLA SEMESTRAL ***         |"
+    echo "|       Base (I=interna, E=externa)     :  " $base
+    echo "|       Formato(1=1-11,2=1-11-45)       :  " $entfmt
+    echo "|       Fecha de Proceso                :  " $fecha
+    echo "+-----------------------------------------------------------+"
+    echo "Si desea modificar algun parametro digite S ---> \c"
+    read modpard
+    if test -z "$modpard"
+    then
+        modpar="N"
     else
-       modpar="N"
+        if [ $modpard == "s" ] || [ $modpard == "S" ]; then
+            modpar="S"
+        else
+            modpar="N"
+        fi
     fi
-  fi
-  if [ $modpar == "S" ] || [ $modpar == "s" ]; then
-     modificar_parametros
-  fi
+    if [ $modpar == "S" ] || [ $modpar == "s" ]; then
+        modificar_parametros
+    fi
 }
 
 #***********************************************************************
@@ -44,73 +81,73 @@ mostrar_parametros() {
 #   Pide parametros y arma el archivo PEfalsemestral
 #***********************************************************************
 pedir_parametros() {
-  regp=""
-  echo "+--------------------------------------------------+"
-  echo "| La base de entrada puede ser:                    |"
-  echo "|         Interna Datacredito (I)                  |"
-  echo "|         o Externa           (E)                  |"
-  echo "Digite tipo de base (I o E)   ---> \c"
-  read base
-  if test -z "$base"
-   then
-    base="I"
-  else
-   if [ $base = "E" ] || [ $base = "e" ]; then
-      base="E"
-   else
-      base="I"
-   fi
-  fi
-  reg=$regp$base";"
-  regp=$reg
-#
-  echo "+--------------------------------------------------+"
-  if [ $base = "E" ]; then
-   echo "+--------------------------------------------------+"
-   echo "|El archivo de entrada puede tener formato:        |"
-   echo "|   1 - 11          (Digitar : 1)                  |"
-   echo "|   1 - 11 - 45     (Digitar : 2)                  |"
-   echo "Digite formato del archivo  ---> \c"
-   read entfmt
-   if test -z "$entfmt"
+    regp=""
+    echo "+--------------------------------------------------+"
+    echo "| La base de entrada puede ser:                    |"
+    echo "|         Interna Datacredito (I)                  |"
+    echo "|         o Externa           (E)                  |"
+    echo "Digite tipo de base (I o E)   ---> \c"
+    read base
+    if test -z "$base"
     then
-     entfmt=1
-   else
-    if [ $entfmt -gt 2 ]; then
-     entfmt=2
+        base="I"
     else
-      if [ $entfmt -lt 1 ]; then
-       entfmt=1
-      fi
+        if [ $base = "E" ] || [ $base = "e" ]; then
+            base="E"
+        else
+            base="I"
+        fi
     fi
-   fi
-  else
-   entfmt=1
-  fi
-  reg=$regp$entfmt";"
-  regp=$reg
-  echo "+--------------------------------------------------+"
-  echo "Si fecha de proceso no es la actual, digite fecha (AAAAMMDD) ---> \c"
-  read fechad
-  if test -z "$fechad"
-   then fecha=$fechah
-  else
-   if [ $fechah -gt $fechad ] || [ $fechah -eq $fechad ]; then
-    fecha=$fechad
-   else
-    echo "fecha digitada es invalida ... se asume fecha de hoy "
-    fecha=$fechah
-   fi
-  fi
-  reg=$regp$fecha";"
-  regp=$reg
+    reg=$regp$base";"
+    regp=$reg
+    #
+    echo "+--------------------------------------------------+"
+    if [ $base = "E" ]; then
+        echo "+--------------------------------------------------+"
+        echo "|El archivo de entrada puede tener formato:        |"
+        echo "|   1 - 11          (Digitar : 1)                  |"
+        echo "|   1 - 11 - 45     (Digitar : 2)                  |"
+        echo "Digite formato del archivo  ---> \c"
+        read entfmt
+        if test -z "$entfmt"
+        then
+            entfmt=1
+        else
+            if [ $entfmt -gt 2 ]; then
+                entfmt=2
+            else
+                if [ $entfmt -lt 1 ]; then
+                    entfmt=1
+                fi
+            fi
+        fi
+    else
+        entfmt=1
+    fi
+    reg=$regp$entfmt";"
+    regp=$reg
+    echo "+--------------------------------------------------+"
+    echo "Si fecha de proceso no es la actual, digite fecha (AAAAMMDD) ---> \c"
+    read fechad
+    if test -z "$fechad"
+    then fecha=$fechah
+    else
+        if [ $fechah -gt $fechad ] || [ $fechah -eq $fechad ]; then
+            fecha=$fechad
+        else
+            echo "fecha digitada es invalida ... se asume fecha de hoy "
+            fecha=$fechah
+        fi
+    fi
+    reg=$regp$fecha";"
+    regp=$reg
 
-  fecha6=`echo $fecha  | cut -c1-6`
-  echo $reg >| $cadena01.parm
-  echo "Se grabo el registro de parametros"
-  echo $fecha
-  mostrar_parametros
-#
+    fecha6=`echo $fecha  | cut -c1-6`
+    echo $reg >| $cadena01.parm
+    echo "Se grabo el registro de parametros"
+    echo $fecha
+    mostrar_parametros
+    #
 }
 
 #***********************************************************************
@@ -118,80 +155,80 @@ pedir_parametros() {
 #   Pide los parametros a modificar y graba el archivo ventacr$codsus  *
 #***********************************************************************
 modificar_parametros() {
-  echo "Modificar parametros"
-  regp=""
-  echo "+--------------------------------------------------+"
-  echo "| La base de entrada puede ser:                    |"
-  echo "|         Interna Datacredito (I)                  |"
-  echo "|         o Externa           (E)                  |"
-  echo "Digite tipo de base (I o E)   ---> \c"
-  read based
-  if test -z "$based"
-   then
-    reg=$regp$base";"
-    regp=$reg
-  else
-   if [ $based = "E" ] || [ $based = "e" ]; then
-      based="E"
-   else
-      based="I"
-   fi
-   reg=$regp$based";"
-   regp=$reg
-   base=$based
-  fi
-#
-  echo "+--------------------------------------------------+"
-  if [ $base = "E" ]; then
-   echo "+--------------------------------------------------+"
-   echo "|El archivo de entrada puede tener formato:        |"
-   echo "|   1 - 11          (Digitar : 1)                  |"
-   echo "|   1 - 11 - 45     (Digitar : 2)                  |"
-   echo "Digite formato del archivo  ---> \c"
-   read entfmtd
-   if test -z "$entfmtd"
+    echo "Modificar parametros"
+    regp=""
+    echo "+--------------------------------------------------+"
+    echo "| La base de entrada puede ser:                    |"
+    echo "|         Interna Datacredito (I)                  |"
+    echo "|         o Externa           (E)                  |"
+    echo "Digite tipo de base (I o E)   ---> \c"
+    read based
+    if test -z "$based"
     then
-     reg=$regp$entfmt";"
-     regp=$reg
-   else
-    if [ $entfmtd -gt 3 ]; then
-     entfmtd=3
+        reg=$regp$base";"
+        regp=$reg
     else
-      if [ $entfmtd -lt 1 ]; then
-       entfmtd=1
-      fi
+        if [ $based = "E" ] || [ $based = "e" ]; then
+            based="E"
+        else
+            based="I"
+        fi
+        reg=$regp$based";"
+        regp=$reg
+        base=$based
     fi
-    reg=$regp$entfmtd";"
-    regp=$reg
-    entfmt=$entfmtd
-   fi
-  else
-   entfmt=1
-   reg=$regp$entfmt";"
-   regp=$reg
-  fi
-  echo "+--------------------------------------------------+"
-  echo "Si va a modificar fecha de proceso digite fecha (AAAAMMDD) ---> \c"
-  read fechad
-  if test -z "$fechad"
-   then
-     reg=$regp$fecha";"
-     regp=$reg
-  else
-   if [ $fechah -gt $fechad ]; then
-    fecha=$fechad
-   else
-    fecha=$fechah
-   fi
-   reg=$regp$fecha";"
-   regp=$reg
-  fi
-  fecha6=`echo $fecha  | cut -c1-6`
+    #
+    echo "+--------------------------------------------------+"
+    if [ $base = "E" ]; then
+        echo "+--------------------------------------------------+"
+        echo "|El archivo de entrada puede tener formato:        |"
+        echo "|   1 - 11          (Digitar : 1)                  |"
+        echo "|   1 - 11 - 45     (Digitar : 2)                  |"
+        echo "Digite formato del archivo  ---> \c"
+        read entfmtd
+        if test -z "$entfmtd"
+        then
+            reg=$regp$entfmt";"
+            regp=$reg
+        else
+            if [ $entfmtd -gt 3 ]; then
+                entfmtd=3
+            else
+                if [ $entfmtd -lt 1 ]; then
+                    entfmtd=1
+                fi
+            fi
+            reg=$regp$entfmtd";"
+            regp=$reg
+            entfmt=$entfmtd
+        fi
+    else
+        entfmt=1
+        reg=$regp$entfmt";"
+        regp=$reg
+    fi
+    echo "+--------------------------------------------------+"
+    echo "Si va a modificar fecha de proceso digite fecha (AAAAMMDD) ---> \c"
+    read fechad
+    if test -z "$fechad"
+    then
+        reg=$regp$fecha";"
+        regp=$reg
+    else
+        if [ $fechah -gt $fechad ]; then
+            fecha=$fechad
+        else
+            fecha=$fechah
+        fi
+        reg=$regp$fecha";"
+        regp=$reg
+    fi
+    fecha6=`echo $fecha  | cut -c1-6`
 
-  echo $reg >| $cadena01.parm
-  echo "Se modifico el registro de parametros"
-  mostrar_parametros
-#
+    echo $reg >| $cadena01.parm
+    echo "Se modifico el registro de parametros"
+    mostrar_parametros
+    #
 }
 
 #***********************************************************************
@@ -199,27 +236,26 @@ modificar_parametros() {
 # extrae registros por suscriptor del archivo ICMCRECOPY.DAT
 #***********************************************************************
 ejecutar_extraccion() {
-  P_SERVER_DEV="172.24.6.154"
-  P_MAQUINA=`who am i | cut -c 39-50`
-  echo "MAQUINA: " $P_MAQUINA
-  if [ $P_MAQUINA == $P_SERVER_DEV ] ; then
-       echo "MAQUINA DE DESARROLLO" $P_MAQUINA
-       cd /despeciales
-  else
-       echo "MAQUINA DE PRODUCCION : " $P_MAQUINA
-       cd $ESPECIALES/ctlc
-  fi
-  grep "^A......[14].*"$nitsus".P$" ICMCRECOPY.DAT | cut -c8-19 | sort -u >| $cadena01                    
-  numeroreg1=`wc -l < $cadena01`
-  mv $cadena01 $TEMPORALES/$cadena01
-  entfmt=1
-  cd $TEMPORALES
-#
+    P_SERVER_DEV="172.24.6.154"
+    P_MAQUINA=`who am i | cut -c 39-50`
+    echo "MAQUINA: " $P_MAQUINA
+    if [ $P_MAQUINA == $P_SERVER_DEV ] ; then
+        echo "MAQUINA DE DESARROLLO" $P_MAQUINA
+        cd /despeciales
+    else
+        echo "MAQUINA DE PRODUCCION : " $P_MAQUINA
+        cd $ESPECIALES/ctlc
+    fi
+    grep "^A......[14].*"$NITSuscriptor".P$" ICMCRECOPY.DAT | cut -c8-19 | sort -u >| $cadena01                    
+    numeroreg1=`wc -l < $cadena01`
+    mv $cadena01 $TEMPORALES/$cadena01
+    entfmt=1
+    cd $TEMPORALES
+    #
 }
 
 #PROGRAMA PRINCIPAL
-nitsus=900047981
-#Fecha de ejecuciÃ³n
+#Fecha de ejecución
 fechah=`date '+%Y%m%d'`
 fechah6=`echo $fechah  | cut -c1-6`
 #
